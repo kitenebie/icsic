a1da1fb1ca5b26f73b033ba1bdc95e32c178f34e
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-11 12:57:38 +0800

    fix domain

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable implements FilamentUser
      */
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
     }
     protected $fillable = [
         'FirstName',
