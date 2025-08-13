a30716a98ce88fd47e86c84aaa49540ddf9c59cf
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-13 21:52:44 +0800

    test

--- resources/views/livewire/auth/forgot-password.blade.php
+++ resources/views/livewire/auth/forgot-password.blade.php
@@ -44,12 +44,12 @@ new #[Layout('components.layouts.auth')] class extends Component {
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
 
-    <form  class="flex flex-col gap-6">
+    <form wire:submit="sendPasswordResetLink" class="flex flex-col gap-6">
         <!-- Email Address -->
         <flux:input wire:model="email" :label="__('Email Address')" type="email" required autofocus
             placeholder="email@example.com" />
 
-        <flux:button variant="primary" type="button" wire:click="sendPasswordResetLink" class="w-full">{{ __('Email password reset link') }}</flux:button>
+        <flux:button variant="primary" type="submit" class="w-full">{{ __('Email password reset link') }}</flux:button>
     </form>
 
     <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-400">
