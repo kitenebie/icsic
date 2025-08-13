0025116e387500337832bf55a71c1e6876a81900
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-13 19:30:05 +0800

    ignore

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -56,8 +56,7 @@ class StudentResource extends Resource
 
                 TextInput::make('lrn')
                     ->required()
-                    ->unique(ignoreRecord: true) // Allow same value when editing
-                    ->hidden(fn($record) => $record !== null), // Hide on edit
+                    ->unique(ignoreRecord: true)->hidden(fn($record) => $record !== null), // Hide on edit
 
                 TextInput::make('firstname')->required(),
                 TextInput::make('lastname')->required(),
@@ -74,8 +73,7 @@ class StudentResource extends Resource
 
                 TextInput::make('email')
                     ->email()
-                    ->unique(ignoreRecord: true) // Allow same value when editing
-                    ->hidden(fn($record) => $record !== null), // Hide on edit
+                    ->unique(ignoreRecord: true)->hidden(fn($record) => $record !== null), // Hide on edit
 
                 TextInput::make('guardian_name')->required(),
                 TextInput::make('relationship')->required(),
