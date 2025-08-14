d6dbf49f63dc219c1f0b542aee4aae6cf6017a8c
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-14 15:39:03 +0800

    test

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
 
 <title>{{ $title ?? config('app.name') }}</title>
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 <link rel="stylesheet" href="/build/assets/app-DoDyzDhk.css">
