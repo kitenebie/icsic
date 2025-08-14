8f52b85ac122c26482b21aeb388b41e7c05ffce3
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-14 15:55:17 +0800

    test

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,16 +2,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>{{ $title ?? config('app.name') }}</title>
-
-<!-- Fonts (proxied) -->
-<link rel="preconnect" href="{{ url('/') }}">
-<link href="/fonts/instrument-sans.css" rel="stylesheet" />
-
-<!-- Local CSS & JS -->
-<link rel="stylesheet" href="/build/assets/app-DoDyzDhk.css" crossorigin />
-<script src="/build/assets/app.js" type="module" crossorigin></script>
-
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link rel="stylesheet" href="/build/assets/app-DoDyzDhk.css">
+<script src="/build/assets/app.js" type="module"></script>
 {{-- <script src="//unpkg.com/alpinejs" defer></script> --}}
 <x-script.app />
 @fluxAppearance
 @livewireStyles()
+<!-- index.html -->
