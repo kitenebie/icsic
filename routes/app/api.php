a2f4cbace133cfd832e7bc3dae07659533e258c9
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-18 19:49:51 +0800

    update

--- routes/app/api.php
+++ routes/app/api.php
@@ -3,4 +3,4 @@ use App\Http\Controllers\SmsAPIController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/api/sms/', [SmsAPIController::class, 'getDATA']);
-Route::get('/api/sms/{id}/update', [SmsAPIController::class, 'update']);
+Route::get('/api/sms/{id}/update/${status}', [SmsAPIController::class, 'update']);
