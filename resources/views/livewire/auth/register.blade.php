0bcd3c790d6167e9a9f48c7fd03a06d86c2d9af8
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-18 20:11:09 +0800

    update

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -38,7 +38,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
         event(new Registered(($user = User::create($validated))));
 
         Auth::login($user);
-        $this->redirect('/');
+        $this->redirect('/waiting');
     }
 }; ?>
 
