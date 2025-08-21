02c94e147385586cc626a9fbb762e4dcc00ade5b
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-21 13:34:46 +0800

    remove body styel

--- resources/views/main/announcement.blade.php
+++ resources/views/main/announcement.blade.php
@@ -3,13 +3,6 @@
     <script src="/build/js/announcement.js"></script>
     {{-- @livewire('announcement.comments') --}}
     <style>
-        body {
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-            background-color: #f0f2f5;
-            margin: 0;
-            padding: 0;
-            color: #050505;
-        }
 
         .facebook-newsfeed {
             min-height: 100vh;
