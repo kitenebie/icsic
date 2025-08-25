31083395b3bd8d12f67dfbbd43b93e931586f623
Codego <101083976+kitenebie@users.noreply.github.com>
2025-08-25 15:10:02 +0800

    add json

--- resources/views/livewire/announcement/main.blade.php
+++ resources/views/livewire/announcement/main.blade.php
@@ -132,7 +132,8 @@
                         </button>
 
                         <button class="action-btn"
-                            onclick="sharePost({{ $announcement->id }}, '{{ addslashes($announcement->title) }}', '{{ addslashes(strip_tags($announcement->content)) }}')"
+                            onclick="sharePost({{ $announcement->id }}, '{{ addslashes($announcement->title) }}', 
+    {!! json_encode(strip_tags($announcement->content)) !!})"
                             aria-label="Share post">
                             <i class="fas fa-share"></i>
                             <span class="action-text">Share</span>
