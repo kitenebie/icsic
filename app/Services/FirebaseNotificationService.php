143c5572d76c8b10598ab5a927353f38e040e223
Codego <101083976+kitenebie@users.noreply.github.com>
2025-09-04 14:33:44 +0800

    test

--- app/Services/FirebaseNotificationService.php
+++ app/Services/FirebaseNotificationService.php
@@ -12,7 +12,7 @@ class FirebaseNotificationService
 
     public function __construct()
     {
-        $this->credentialsPath = storage_path('app/firebase/firebase_credentials.json');
+        $this->credentialsPath = "https://irosincentralschool.com/firebase-messaging-sw.js";
     }
 
     protected function getAccessToken()
