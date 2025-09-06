eca859646caa014901eed1bdf8a0f3549ba0b418
Codego <101083976+kitenebie@users.noreply.github.com>
2025-09-06 19:08:27 +0800

    dsad

--- resources/views/livewire/event/main.blade.php
+++ resources/views/livewire/event/main.blade.php
@@ -6,7 +6,7 @@
             <x-filament::button
                 style="background-color:#a5f8c5; color: #054721; display: flex; border: none; padding: 8px 16px; border-radius: 8px; font-weight: 500; cursor: pointer; display: flex; align-items: center; gap: 8px; transition: background-color 0.2s; width: fit-content;"
                 class="dark:bg-gray-700 dark:text-green-400 dark:hover:bg-gray-600"
-                onmouseover="this.style.backgroundColor='#62E489FF'" onmouseout="this.style.backgroundColor='#449C65'">
+                onmouseover="this.style.backgroundColor='#62E489FF'" onmouseout="this.style.backgroundColor='#a5f8c5'">
                 <svg style="width: 20px; height: 20px;" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                 </svg>
