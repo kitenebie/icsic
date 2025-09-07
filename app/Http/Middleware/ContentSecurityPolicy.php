1e9259bf85de3102193ca000316d45cf9c8048bb
Codego <101083976+kitenebie@users.noreply.github.com>
2025-09-07 11:43:39 +0800

    update

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -18,7 +18,19 @@ class ContentSecurityPolicy
         $response = $next($request);
         $response->headers->set(
             'Content-Security-Policy',
-            "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.bunny.net; font-src 'self' https://fonts.bunny.net; script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.gstatic.com/firebasejs"
+            "default-src 'self'; " .
+            "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://www.gstatic.com https://www.gstatic.com/firebasejs https://irosincentralschool.com; " .
+            "style-src 'self' 'unsafe-inline' https://fonts.bunny.net; " .
+            "style-src-elem 'self' 'unsafe-inline' https://fonts.bunny.net; " .
+            "script-src-elem 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " .
+            "img-src 'self' data: https: blob:; " .
+            "font-src 'self' https://fonts.bunny.net; " .
+            "connect-src 'self' https://cdn.jsdelivr.net https://irosincentralschool.com; " .
+            "media-src 'self' blob:; " .
+            "object-src 'none'; " .
+            "frame-src 'none'; " .
+            "base-uri 'self'; " .
+            "form-action 'self';"
         );
         return $response;
     }
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,7 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://www.gstatic.com https://www.gstatic.com/firebasejs https://irosincentralschool.com; style-src 'self' 'unsafe-inline' https://fonts.bunny.net; style-src-elem 'self' 'unsafe-inline' https://fonts.bunny.net; script-src-elem 'self' 'unsafe-inline' https://cdn.jsdelivr.net; img-src 'self' data: https: blob:; font-src 'self' https://fonts.bunny.net; connect-src 'self' https://cdn.jsdelivr.net https://irosincentralschool.com; media-src 'self' blob:;">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
