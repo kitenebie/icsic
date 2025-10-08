7778a5541abbf96067e74e4c1aed95f971bd6ff3
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-08 13:29:28 +0800

    feat(ui): add favicon support
    Add favicon image file and link it in the head partial for better branding and user experience

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,8 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
+<link rel="icon" type="image/png" href="/favicon.png">
+<link rel="shortcut icon" href="/favicon.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
