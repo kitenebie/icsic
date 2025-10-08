f3580cf35c5821f2bd5ac3033d6aa55a8395454e
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-08 14:08:46 +0800

    feat(ui): add max content width to admin panel
    Set the maximum content width to full for the Filament administrator panel to improve layout flexibility.

--- app/Providers/Filament/AdministratorPanelProvider.php
+++ app/Providers/Filament/AdministratorPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Support\Enums\Width;
 
 class AdministratorPanelProvider extends PanelProvider
 {
@@ -31,6 +32,7 @@ class AdministratorPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Green,
             ])
+            ->maxContentWidth( Width::Full)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
