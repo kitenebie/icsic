3512598e5e5d137086b906a4bab7151e22e6f55f
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-13 13:18:28 +0800

    update 3

--- resources/views/livewire/event/event.blade.php
+++ resources/views/livewire/event/event.blade.php
@@ -652,7 +652,7 @@
                                         z-index: 9999;
                                         background: linear-gradient(135deg, #2D9152FF 0%, #15843E 100%);
                                         color: rgb(226, 226, 226);
-                                        font-size: 10px;
+                                        font-size: 16px;
                                         padding: 4px 8px;
                                         border-radius: 6px;
                                         font-weight: 600;
