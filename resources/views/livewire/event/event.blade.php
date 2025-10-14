aa698e9a1e1a2f35f994d4bedddb9ec7e903faf1
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-14 10:56:04 +0800

    djs

--- resources/views/livewire/event/event.blade.php
+++ resources/views/livewire/event/event.blade.php
@@ -533,7 +533,7 @@
                         calendar.innerHTML = "";
                         const year = date.getFullYear();
                         const month = date.getMonth();
-
+                        alert(`Rendering calendar for ${month + 1}/${year}`);
                         const firstDay = new Date(year, month, 1);
                         const lastDay = new Date(year, month + 1, 0);
                         const startDay = firstDay.getDay();
