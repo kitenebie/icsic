88200373bdeae90f5e60f2e5ee34cf7053bea6ce
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-14 09:29:02 +0800

    ks

--- resources/views/livewire/event/event.blade.php
+++ resources/views/livewire/event/event.blade.php
@@ -574,7 +574,7 @@
                                                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                                         </svg>
-                                                        <span>${new Date(e.raw.event_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} | ${e.raw.event_duration ? ' – ' + e.raw.event_duration : ''}</span>
+                                                        <span>${new Date(e.raw.event_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} | ${new Date(e.raw.event_end).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} ${e.raw.event_duration ? ' – ' + e.raw.event_duration : ''}</span>
                                                     </div>
 
                                                     <div class="text-sm text-gray-700 leading-relaxed">
