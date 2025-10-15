2e170e3553cdb15f64d4760b684b00c512fbc457
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-15 17:02:53 +0800

    dsk

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,7 +8,7 @@
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-<link rel="stylesheet" href="/build/assets/app-ChVnFOjA.css">
+{{-- <link rel="stylesheet" href="/build/assets/app-ChVnFOjA.css"> --}}
 <script src="/build/assets/app-BdQ6hJev.js" type="module"></script>
 <x-script.app />
 @fluxAppearance
