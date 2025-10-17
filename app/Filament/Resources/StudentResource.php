f548544ad6bbf62b69c75be10a34eeb7b5d634da
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-17 10:24:10 +0800

    dsal

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -636,7 +636,7 @@ class StudentResource extends Resource
     public static function getEloquentQuery(): Builder
     {
         $query = parent::getEloquentQuery()
-            ->leftJoin('users', 'students.email', '=', 'users.email')
+            ->leftJoin('users', 'students.lrn', '=', 'users.lrn')
             ->select([
                 'students.id',
                 'users.id as userId',
