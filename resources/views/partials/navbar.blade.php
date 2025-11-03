4b738993dbe4dab5730e87b95f92266e86e4c641
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-03 12:45:19 +0800

    i

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="border-b z-50 sticky top-0 bg-white border-gray-200">
+<nav class="border-b z-50 sticky top-0 bg-white border-cream-100">
     <div class="flex items-center justify-between px-6 py-4">
         <!-- Logo -->
         <div class="flex items-center space-x-2">
