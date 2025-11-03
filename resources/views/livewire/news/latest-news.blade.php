830e8fd48931ed677c603470d5290dbe85d33c7d
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-04 07:38:03 +0800

    up

--- resources/views/livewire/news/latest-news.blade.php
+++ resources/views/livewire/news/latest-news.blade.php
@@ -37,9 +37,7 @@
                 <div class="flex items-center justify-between">
                     <div class="flex items-center space-x-3">
                         <div class="w-8 h-8 bg-gradient-to-r from-cream-400 to-brown-500 rounded-full flex items-center justify-center">
-                            <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-                            </svg>
+                            <img class="w-4 h-4 text-white" src="https://irosincentralschool.com/favicon.png" alt="" srcset="">
                         </div>
                         <div>
                             <p class="text-xs font-medium text-gray-900">{{ $News_item->author ?? 'School Admin' }}</p>
