666296a84eb2e41d41dd5f76e8f0fa730631cae9
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-04 08:30:23 +0800

    up

--- resources/views/livewire/event/event.blade.php
+++ resources/views/livewire/event/event.blade.php
@@ -605,7 +605,7 @@
                             // Today indicator
                             if (isToday) {
                                 const todayBadge = document.createElement("div");
-                                todayBadge.className = "absolute top-2 right-2 w-2 h-2 bg-brown-600 rounded-full";
+                                todayBadge.className = "absolute top-4 right-8 w-2 h-2 bg-brown-600 rounded-full";
                                 div.appendChild(todayBadge);
                             }
 
