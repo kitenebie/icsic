14917afd6e0bee1719959c41779feec1b77dafda
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-04 13:57:39 +0800

    xsd

--- resources/views/unable.blade.php
+++ resources/views/unable.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Account Rejected</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="/css/custome.css">
 </head>
 <body class="bg-gray-100 min-h-screen flex items-center justify-center px-4">
   <div class="bg-white max-w-md w-full p-8 rounded-2xl shadow-xl text-center">
