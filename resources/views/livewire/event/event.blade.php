69d2814717471123325451d929270a678b05d6ab
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-04 08:56:53 +0800

    up

--- resources/views/livewire/event/event.blade.php
+++ resources/views/livewire/event/event.blade.php
@@ -5,8 +5,8 @@
             <div class="flex items-center justify-between p-6 border-b border-gray-200">
                 <div class="flex items-center space-x-4">
                     <div class="flex items-center space-x-3">
-                        <div style="background-image: linear-gradient(to right, #ffd18f, #brown) !important;"
-                            class="w-10 h-10 bg-grad-cream-brown-right rounded-lg flex items-center justify-center">
+                        <div style="background: linear-gradient(to right, #ffd18f, #brown) !important;"
+                            class="w-10 h-10 rounded-lg flex items-center justify-center">
                             <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z">
