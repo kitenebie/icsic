09f3a0dd562d9ec6c8363b6857e0f23f46aea7ff
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-04 09:50:47 +0800

    ds

--- app/Livewire/Event/Event.php
+++ app/Livewire/Event/Event.php
@@ -50,8 +50,7 @@ class Event extends Component
 
     public function loadEvents()
     {
-        $this->events = EventModel::where('updated_at', '>=', now())
-            ->orderBy('event_date', 'asc')
+        $this->events = EventModel::orderBy('event_date', 'asc')
             ->get();
     }
     public function render()
