48b33e4cbb0e65c018dd1544c39db0b623b26cf1
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-06 07:34:12 +0800

    dsa

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -149,7 +149,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
     @endif
     @else
         <x-auth-header :title="__('Payment Section')" :description="__('Please complete your payment using GCash.')" />
-        <img src="/gcash.jpg" alt="GCash Logo" style="border-radius: 15px" width="500" height="800">
+        <img src="/gcash1.jpg" alt="GCash Logo" style="border-radius: 15px" width="3rem" height="3rem">
     @endif
 </div>
 
