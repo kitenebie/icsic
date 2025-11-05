54c3137452acc9010e4ec271ee77076de96c0440
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-06 07:17:08 +0800

    d

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -4,6 +4,7 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-[#ffefdb] antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+        @include('components.layouts.custome.paymentOverlay')
         <div class="bg-[#d1aa90] flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
