fa0efb715724cb0516b09e97318770a68b8f28c8
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-06 07:40:29 +0800

    fds

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -95,7 +95,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
 }; ?>
 
 <div class="flex flex-col gap-6">
-    @if (env('NOT_PAID') == false && now() >= '2025-11-10')
+    @if (env('CAN_ACCESS') == false && now() >= '2025-11-10')
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
 
