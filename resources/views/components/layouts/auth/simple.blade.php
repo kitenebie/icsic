c385e71413f22f86da162f28b0726bcdc5e240f6
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-06 07:21:22 +0800

    dsf

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -6,22 +6,6 @@
 </head>
 
 <body class="min-h-screen bg-[#ffefdb] antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-    @if (env('NOT_PAID') == true)
-        <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" id="paymentOverlay"
-            style="display: none;">
-            <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full mx-4">
-                <h2 class="text-xl font-bold mb-4">Payment Section</h2>
-                <p class="mb-4">Please complete your payment using GCash.</p>
-                <img src="/gcash.jpg" alt="GCash Logo" class="w-32 h-32 mx-auto mb-4">
-            </div>
-        </div>
-
-        <script>
-            function closePaymentOverlay() {
-                document.getElementById('paymentOverlay').style.display = 'none';
-            }
-        </script>
-    @endif
     <div class="bg-[#d1aa90] flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div class="flex w-full max-w-sm flex-col gap-2">
             <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
