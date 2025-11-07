e99aed6eeef8ce02ec97553c05b1c69657a7920e
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-08 07:54:55 +0800

    dsfsd

--- resources/views/components/layouts/custome/header.blade.php
+++ resources/views/components/layouts/custome/header.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ darkMode: $persist(false) }" :class="{ 'dark': darkMode }">
 
 <head>
     <meta charset="utf-8">
