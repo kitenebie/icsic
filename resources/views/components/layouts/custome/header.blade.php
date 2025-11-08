143daa8543f9c44ec91e043671662b0e1ddddfdd
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-08 08:03:58 +0800

    djfshb

--- resources/views/components/layouts/custome/header.blade.php
+++ resources/views/components/layouts/custome/header.blade.php
@@ -14,6 +14,18 @@
     <script src="/build/assets/app.js"></script>
     <x-script.app />
     <style>
+        :root,
+        html.light,
+        [data-theme="light"] {
+            color-scheme: light !important;
+        }
+
+        .dark {
+            --tw-bg-opacity: 1;
+            background-color: rgb(255 255 255 / var(--tw-bg-opacity));
+            color: black !important;
+        }
+
         body {
             font-family: "Inter", sans-serif;
         }
