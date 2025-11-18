e5f73d8d77386551a1dda96e607a1ce80b79ba05
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-18 15:03:00 +0800

    dssc

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -723,7 +723,7 @@ class StudentResource extends Resource
                         // Update the student data
                         // dd($studentModel);
                         try {
-                            if ($studentModel->user_email !== $data['email']) {
+                            if ($studentModel->user_email !== $data['email'] || $studentModel->guardian_contact_number !== $data['email']) {
                                 if (Student::where('guardian_contact_number', $data['email'])->exists() || User::where('email', $data['guardian_contact_number'])->exists() || User::where('email', $data['email'])->exists() || Student::where('email', $data['email'])->exists()) {
                                     Notification::make()
                                         ->title('Email is already Exist')
