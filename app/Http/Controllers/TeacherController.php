4252474b003f198f4d561fa755e1730127c066b1
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-21 09:21:02 +0800

    ,lcd

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Group;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TeacherController extends Controller
 {
@@ -12,7 +14,33 @@ class TeacherController extends Controller
         $teachers = User::where('role', 'teacher')
             ->select('id', 'grade', 'section')
             ->get();
+        foreach ($teachers as $teacher) {
+            $teacher->groups = [];
+            if ($teacher->grade && $teacher->section) {
+                $groupName = $teacher->grade . '- Section ' . $teacher->section;
+                $ParentgroupName = 'Parents - '.$teacher->grade . '- Section ' . $teacher->section;
 
-        return response()->json($teachers);
+                // Find or create the group
+                $group = Group::firstOrCreate(
+                    ['name' => $groupName],
+                    ['author_id' => 1]
+                );
+                $group2 = Group::firstOrCreate(
+                    ['name' => $ParentgroupName],
+                    ['author_id' => 1]
+                );
+
+                $parent_user_group[] = $group->id;
+                $parent_user_group2[] = $group2->id;
+            }
+        }
+        User::where('id', Auth::user()->id)->update(['user_group' => $parent_user_group]);
+        User::where('id', Auth::user()->id)->update(['user_group' => $parent_user_group2]);
+
+        return response()->json([
+            'teachers' => $teachers,
+            'all_user_group' => $parent_user_group,
+            'parent_user_group2' => $parent_user_group2,
+        ]);
     }
 }
