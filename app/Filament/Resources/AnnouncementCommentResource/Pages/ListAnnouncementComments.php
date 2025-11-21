d843f4e3fc0bb365abb4f9f62ea9fb55ead0b62d
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-22 07:55:25 +0800

    update

--- app/Filament/Resources/AnnouncementCommentResource/Pages/ListAnnouncementComments.php
+++ app/Filament/Resources/AnnouncementCommentResource/Pages/ListAnnouncementComments.php
@@ -13,7 +13,7 @@ class ListAnnouncementComments extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
