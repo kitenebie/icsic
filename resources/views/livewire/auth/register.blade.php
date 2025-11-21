192dc8783d10fec9d4ca201d6760cc5f6ea47784
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-21 10:40:15 +0800

    dsd

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -215,7 +215,7 @@ new #[Layout('components.layouts.auth')] class extends Component {}; ?>
         </div>
     </div>
         <!-- First Name -->
-        <flux:input name="FirstName" :label="__('First Name')" type="text" required autofocus
+        <flux:input name="FirstName" :label="__('First Name') . '<span class=\"text-red-500\">*</span>'" type="text" required autofocus
             autocomplete="FirstName" :placeholder="__('First name')" />
         <!-- Last Name -->
         <flux:input name="LastName" :label="__('Last Name')" type="text" required autocomplete="LastName"
@@ -227,23 +227,23 @@ new #[Layout('components.layouts.auth')] class extends Component {}; ?>
         <flux:input name="extension_name" :label="__('Ext Name')" type="text" autocomplete="extension_name"
             :placeholder="__('Ext name')" />
         <!-- conact -->
-        <flux:input name="contact" :label="__('Contact Number')" type="number" autocomplete="contact"
+        <flux:input name="contact" :label="__('Contact Number') . '<span class=\"text-red-500\">*</span>'" type="number" autocomplete="contact"
             :placeholder="__('Contact Number')" />
         <!-- Email Address -->
-        <flux:input name="email" :label="__('Email address')" type="email" required autocomplete="email"
+        <flux:input name="email" :label="__('Email address') . '<span class=\"text-red-500\">*</span>'" type="email" required autocomplete="email"
             placeholder="email@example.com" />
 
         <!-- Front ID -->
         <div>
-            <label for="front_id" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Front ID</label>
+            <label required for="front_id" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Front ID <span class="text-red-500">*</span></label>
             <input type="file" name="front_id" id="front_id" accept="image/*"
                 class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
         </div>
 
         <!-- Back ID -->
         <div>
-            <label for="back_id" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Back ID</label>
-            <input type="file" name="back_id" id="back_id" accept="image/*"
+            <label for="back_id" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Back ID <span class="text-red-500">*</span></label>
+            <input required type="file" name="back_id" id="back_id" accept="image/*"
                 class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
         </div>
 
