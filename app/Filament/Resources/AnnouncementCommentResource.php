a71f97ede60df41345428c6788b009a8fc689237
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-22 10:18:18 +0800

    dsd

--- app/Filament/Resources/AnnouncementCommentResource.php
+++ app/Filament/Resources/AnnouncementCommentResource.php
@@ -96,7 +96,7 @@ class AnnouncementCommentResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
