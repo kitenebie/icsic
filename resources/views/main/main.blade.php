8d36295d53f196ea42eb1589cca5dc8ba9e2b0b5
Codego <101083976+kitenebie@users.noreply.github.com>
2025-12-28 10:52:18 +0800

    dsdsd

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -22,11 +22,11 @@
                 @if (!auth()->check())
                     <div class="mt-6 flex space-x-3">
                         <a href="/register"
-                            class="bg-brown-500 text-white text-lg px-4 py-12 sm:text-sm font-semibold sm:px-4 sm:py-2 rounded-md hover:bg-brown-700">
+                            class="bg-brown-500 text-white text-sm px-4 py-12 lg:text-lg font-semibold lg:px-4 lg:py-2 rounded-md hover:bg-brown-700">
                             Create an account
                         </a>
                         <a href="{{ route('faq') }}"
-                            class="bg-brown-500 text-white text-lg px-4 py-12 sm:text-sm font-semibold sm:px-4 sm:py-2 rounded-md hover:bg-brown-700">
+                            class="bg-brown-500 text-white text-sm px-4 py-12 lg:text-lg font-semibold lg:px-4 lg:py-2 rounded-md hover:bg-brown-700">
                             Frequently Asked Questions
                         </a>
                     </div>
